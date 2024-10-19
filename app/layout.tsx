ec6b035cd0c4f0f80a98d24843f048e88fc7d13a
Gabriel Alfonso <gabrielalfonso@Gabriels-MacBook-Pro.local>
2024-10-19 11:31:53 -0500

    added image for opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,22 @@ export const metadata: Metadata = {
   title: "Olympico Action Sports",
   description:
     "Welcome to Olympico Action Sport, your premier destination for all things rollerblading and running! As a vibrant community organization and school, we promote active lifestyles through engaging programs, expert coaching, and thrilling events. Join us to elevate your skills, connect with fellow enthusiasts, and embrace the excitement of action sports!",
+  openGraph: {
+    title: "Olympico Action Sports",
+    description:
+      "Discover and join exciting action sports events with Olympico",
+    siteName: "Olympico Action Sports",
+    type: "website",
+    url: "https://www.olympicoactionsports.com", // Replace with your actual URL
+    images: [
+      {
+        url: "/We.Are.Diffrent.jpg",
+        width: 1200, // Replace with the actual width of your image
+        height: 630, // Replace with the actual height of your image
+        alt: "We Are Different - Olympico Action Sports",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -46,10 +62,6 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon_io/favicon-16x16.png"
         />
-        <meta property="og:image" content="<generated>" />
-        <meta property="og:image:type" content="<generated>" />
-        <meta property="og:image:width" content="<generated>" />
-        <meta property="og:image:height" content="<generated>" />
         <link rel="manifest" href="/favicon_io/site.webmanifest" />
       </head>
       <body
